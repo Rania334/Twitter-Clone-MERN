4d04c844761d15246c837653fc0af925d783ac60
rania <ranianour334@gmail.com>
2025-05-04 07:35:29 +0300

    Add server files

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ const __dirname = dirname(__filename);
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
