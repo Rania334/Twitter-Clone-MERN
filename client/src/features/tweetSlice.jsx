3a9e18f4488ef0cf2eba52b86024a802bf55a369
rania <ranianour334@gmail.com>
2025-05-05 00:27:17 +0300

    -

--- client/src/features/tweetSlice.jsx
+++ client/src/features/tweetSlice.jsx
@@ -98,7 +98,7 @@ const tweetSlice = createSlice({
       .addCase(retweetTweet.fulfilled, (state, action) => {
         const updated = action.payload;
         state.tweets = state.tweets.map(t =>
-          t._id === updated._id ? { ...t, likes: updated.retweet } : t
+          t._id === updated._id ? { ...t, likes: updated.retweets } : t
 
         );
       });
