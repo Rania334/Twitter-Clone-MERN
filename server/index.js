558a173a8e62bda56ddfef4fbcbb163f5725e037
rania <ranianour334@gmail.com>
2025-05-04 07:21:25 +0300

    Add server files

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-import express from "express";
+const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
 
-import authRoutes from "./routes/authRoutes.js";
+
+const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const tweetRoutes = require("./routes/tweetRoutes");
 const commentRoutes = require("./routes/commentRoutes");
@@ -15,7 +16,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
@@ -42,14 +42,13 @@ app.use("/api/user", userRoutes);
 app.use("/api/tweet", tweetRoutes);
 app.use("/api/comment", commentRoutes);
 
-// ------------------- Serve frontend in production --------------------
 const __dirname = path.resolve(); // needed if using ES5 `require`
 app.use(express.static(path.join(__dirname, "/client/dist"))); // or "client/build" for CRA
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
-// ---------------------------------------------------------------------
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+z
