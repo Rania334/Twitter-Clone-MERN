41ac5e71aef50de3ede11b990122341ea0660ddc
rania <ranianour334@gmail.com>
2025-05-04 07:33:01 +0300

    Add server files

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ app.use("/api/comment", commentRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
